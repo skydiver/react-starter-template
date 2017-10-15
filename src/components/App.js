958441281bbb1529a59d4e4ea05d6079209ed19f
Martin <skydiver@users.noreply.github.com>
2017-10-15 02:24:13 -0300

    * load app component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <h1>React Starter Template</h1>
+        );
+    }
+
+
+}
+
+export default App;
